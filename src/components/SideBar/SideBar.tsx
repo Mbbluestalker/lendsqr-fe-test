b0381564b61177ecbe3fcddf7e8d8ec6552aa2f0
Akorede Olawole <akoredeolawole@gmail.com>
2022-11-13 22:18:26 +0100

    bugfix: fixed sidebar scroll

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -3,9 +3,12 @@ import "./SideBar.scss";
 import BriefCaseIcon from "../../assets/briefcase 1.svg";
 import ArrowDown from "../../assets/arrowdown2.svg";
 import HomeIcon from "../../assets/home 1.svg";
-import { customerLinks } from "../../data/sidebarData";
-
-
+import LogOut from "../../assets/sign-out 1.svg";
+import {
+  customerLinks,
+  businessesLinks,
+  settingsLinks,
+} from "../../data/sidebarData";
 
 export const SideBar = () => {
   return (
@@ -22,26 +25,48 @@ export const SideBar = () => {
       </div>
 
       <section className="customers">
+        <h3>Customers</h3>
         {customerLinks.map((link: any) => {
           const { id, icon, title } = link;
           return (
-            <div key={id} className="customer">
+            <div key={id} className="customer sidebar-link">
               <img src={icon} alt="" />
               <p>{title}</p>
-
             </div>
-          )
+          );
         })}
       </section>
 
       <section className="businesses">
-
+        <h3>Businesses</h3>
+        {businessesLinks.map((link: any) => {
+          const { id, icon, title } = link;
+          return (
+            <div key={id} className="business sidebar-link">
+              <img src={icon} alt="" />
+              <p>{title}</p>
+            </div>
+          );
+        })}
       </section>
 
       <section className="settings">
-        
+        <h3>Settings</h3>
+        {settingsLinks.map((link: any) => {
+          const { id, icon, title } = link;
+          return (
+            <div key={id} className="business sidebar-link">
+              <img src={icon} alt="" />
+              <p>{title}</p>
+            </div>
+          );
+        })}
       </section>
 
+      <section className="logout">
+        <img src={LogOut} alt="" />
+        <p>Logout</p>
+      </section>
     </div>
   );
 };
--- src/data/sidebarData.ts
+++ src/data/sidebarData.ts
@@ -2,6 +2,22 @@ import HomeIcon from '../assets/guarantors.svg'
 import UserIcon from '../assets/user-friends 1.svg'
 import SackIcon from '../assets/sack 1.svg'
 import DecisionModelIcon from '../assets/decision_models.svg'
+import SavingsIcon from '../assets/piggy-bank 1.svg'
+import LoanIcon from '../assets/loan_products.svg'
+import WhiteListIcon from '../assets/whitelist.svg'
+import Karma from '../assets/karma.svg'
+import BriefCaseIcon from '../assets/briefcase 1.svg'
+import SavingsProducts from '../assets/savings_products.svg'
+import FeesAndCharges from '../assets/coins-solid 1.svg'
+import Transactions from '../assets/transactions.svg'
+import Services from '../assets/services.svg'
+import ServiceAccount from '../assets/service_account.svg'
+import Settlements from '../assets/settlements.svg'
+import Reports from '../assets/chart-bar 2.svg'
+import Preferences from '../assets/preferences.svg'
+import FeesAndPricing from '../assets/badge-percent 1.svg'
+import AuditLogs from '../assets/audit_logs.svg'
+import SystemMessages from '../assets/system_messages.svg'
 
 
 export const customerLinks: any  =[
@@ -39,29 +55,136 @@ export const customerLinks: any  =[
         id: 5,
         title: 'Savings',
         path: '/savings',
-        icon: DecisionModelIcon
+        icon: SavingsIcon
 
     },
     {
         id: 6,
         title: 'Loan Requests',
         path: '/loan_requests',
-        icon: DecisionModelIcon
+        icon: LoanIcon
 
     },
     {
-        id: 4,
+        id: 7,
         title: 'Whitelist',
         path: '/whitelist',
-        icon: DecisionModelIcon
+        icon: WhiteListIcon
+
+    },
+    {
+        id: 8,
+        title: 'Karma',
+        path: '/karma',
+        icon: Karma
+
+    },
+
+]
+
+
+export const businessesLinks: any  =[
+
+    {
+        id: 1,
+        title: 'Organization',
+        path: '/organization',
+        icon: BriefCaseIcon
+
+    },
+    {
+        id: 2,
+        title: 'Loan Products',
+        path: '/loan_products',
+        icon: LoanIcon
+
+    },
+
+    {
+        id: 3,
+        title: 'Savings Products',
+        path: '/savings_products',
+        icon: SavingsProducts
 
     },
     {
         id: 4,
-        title: 'Karma',
-        path: '/karma',
-        icon: DecisionModelIcon
+        title: 'Fees and Charges', 
+        path: '/fees_and_charges',
+        icon: FeesAndCharges
 
     },
+    {
+        id: 5,
+        title: 'Transactions',
+        path: '/transactions',
+        icon: Transactions
+
+    },
+    {
+        id: 6,
+        title: 'Services',
+        path: '/services',
+        icon: Services
+
+    },
+    {
+        id: 7,
+        title: 'Service Account',
+        path: '/service_account',
+        icon: ServiceAccount
+
+    },
+    {
+        id: 8,
+        title: 'Settlements',
+        path: '/settlements',
+        icon: Settlements
+
+    },
+
+    {
+        id: 9,
+        title: 'Reports',
+        path: '/reports',
+        icon: Reports
+
+    },
+
+]
+
+
+export const settingsLinks: any  =[
+
+    {
+        id: 1,
+        title: 'Preferences',
+        path: '/preferences',
+        icon: Preferences
+
+    },
+    {
+        id: 2,
+        title: 'Fees and Pricing',
+        path: '/fees_and_pricing',
+        icon: FeesAndPricing
+
+    },
+
+    {
+        id: 3,
+        title: 'Audit Logs',
+        path: '/audit_logs',
+        icon: AuditLogs
+
+    },
+
+    {
+        id: 3,
+        title: 'Systems Messages',
+        path: '/systems_messages',
+        icon: SystemMessages
+
+    }
 
 ]
