82fe326bfcc151683bbedda540bd06cc30a2bffd
Akorede Olawole <akoredeolawole@gmail.com>
2022-11-16 00:04:40 +0100

    created user and user details pages

---
+++ src/pages/Users/Users.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import { UserCard, UserDetails } from "../../components";
+
+export const Users = () => {
+  const [users, setUsers] = useState([]);
+  const fetchUsers = async () => {
+    const response = await axios.get(
+      "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
+    );
+    console.log(response.data);
+    setUsers(response.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <UserCard />
+      <UserDetails users={users} />
+    </div>
+  );
+};
